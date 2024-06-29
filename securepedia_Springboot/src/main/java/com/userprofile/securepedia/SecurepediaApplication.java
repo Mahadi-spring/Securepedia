f3b2e50d2ade68a270d467c534f87cc49f3ab5fb
Mahadi-spring <mahadi.spring.developer@gmail.com>
2024-06-29 23:43:45 +0600

    Initial commit

---
+++ securepedia_Springboot/src/main/java/com/userprofile/securepedia/SecurepediaApplication.java
@@ -0,0 +1,13 @@
+package com.userprofile.securepedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurepediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurepediaApplication.class, args);
+	}
+
+}
---
+++ securepedia_Springboot/src/test/java/com/userprofile/securepedia/SecurepediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userprofile.securepedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurepediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
