3895848a54ffad1916ba4d62fc040258d32fc870
Mahadi-spring <mahadi.spring.developer@gmail.com>
2024-07-02 23:13:29 +0600

    Updated

--- securepedia_Springboot/src/main/java/com/userprofile/securepedia/SecurecapitaApplication.java
+++ securepedia_Springboot/src/main/java/com/userprofile/securepedia/SecurecapitaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurepediaApplication {
+public class SecurecapitaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurepediaApplication.class, args);
+		SpringApplication.run(SecurecapitaApplication.class, args);
 	}
 
 }
